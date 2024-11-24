c6e2752c1b8c806d4be4529608bd5a1ac45a699d
anchalsehrawat <anchalsingh987@gmail.com>
2024-11-24 09:07:14 +0530

    script: use local manifest instead of cherry-picks
    Cherry-picking cause conflits if ran in same repo again.
    Better to use local manifest with remove-project tag to handle edited
    repos manually.

--- build.sh
+++ build.sh
@@ -29,25 +29,24 @@ echo "---------------BUILD ENVIRONMENT------------------"
 #croot
 
 #system_core
-cd system/core
-git fetch https://github.com/anchalsehrawat/rising_android_system_core_new.git && git cherry-pick 34506a6916549d5c23fa1b1978059c558fafde2c
-croot
+#cd system/core
+#git fetch https://github.com/anchalsehrawat/rising_android_system_core_new.git && git cherry-pick 34506a6916549d5c23fa1b1978059c558fafde2c
+#croot
 
 #system_update_engine
-cd system/update_engine
-git fetch https://github.com/anchalsehrawat/rising_android_system_update_engine.git && git cherry-pick c43c0631ebce21cf4d6e1c9022978d1f937e9214 && git cherry-pick eee2b092e4a0cfcfd10466b376f4d6a7c85f87ca
-croot
+#cd system/update_engine
+#git fetch https://github.com/anchalsehrawat/rising_android_system_update_engine.git && git cherry-pick c43c0631ebce21cf4d6e1c9022978d1f937e9214 && git cherry-pick eee2b092e4a0cfcfd10466b376f4d6a7c85f87ca
+#croot
 
 #bootable_recovery
-cd bootable/recovery
-git fetch https://github.com/anchalsehrawat/rising_android_bootable_recovery.git && git cherry-pick 106bbd05ac4640a1876cb7caca13172dcc82f250 && git cherry-pick 778a3bc4c6e56a939d05803ba5ed95280e9a505a
-croot
+#cd bootable/recovery
+#git fetch https://github.com/anchalsehrawat/rising_android_bootable_recovery.git && git cherry-pick 106bbd05ac4640a1876cb7caca13172dcc82f250 && git cherry-pick 778a3bc4c6e56a939d05803ba5ed95280e9a505a
+#croot
 
 #Lunch
 riseup ziti userdebug
-rise b
-
 echo "--------------VANILLA BUILD STARTED--------------"
+rise b
 
 mv out/target/product/ziti/*.zip .
 echo "--------------MOVED VANILLA ZIPS TO ROOT DIRECTORY--------------"
@@ -56,9 +55,8 @@ echo "--------------MOVED VANILLA ZIPS TO ROOT DIRECTORY--------------"
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
 export TARGET_DEFAULT_PIXEL_LAUNCHER=true
-rise b
-
 echo "--------------GAPPS BUILD STARTED--------------"
+rise b
 
 mv out/target/product/ziti/*.zip .
 echo "--------------MOVED GAPPS ZIPS TO ROOT DIRECTORY----------------"
