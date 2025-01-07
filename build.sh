a5c5141396466438ba6311f76fb5b3ddf908ef6c
anchalsehrawat <anchalsingh987@gmail.com>
2025-01-07 18:58:42 +0530

    build: add cherry-pick method

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 #Remove files
 rm -rf .repo/local_manifests
 rm -rf out/target/product/ziti
-#rm -rf hardware/qcom/audio
+rm -rf hardware/lineage/compat
+rm -rf system/core
+rm -rf system/update_engine
+rm -rf bootable/recovery
+rm -rf hardware/qcom-caf/sm8350/audio
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
@@ -25,29 +29,29 @@ echo "---------------BUILD ENVIRONMENT------------------"
 #Cherry-picks
 
 #hardware_lineage_compat
-#cd hardware/lineage/compat
-#git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick  9144e5ff7fc4b634e10d4916a3ff6a3ba746af39
-#croot
+cd hardware/lineage/compat
+git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick  9144e5ff7fc4b634e10d4916a3ff6a3ba746af39
+croot
 
 #system_core
-#cd system/core
-#git fetch https://github.com/anchalsehrawat/rising_android_system_core_new.git && git cherry-pick 34506a6916549d5c23fa1b1978059c558fafde2c
-#croot
+cd system/core
+git fetch https://github.com/anchalsehrawat/rising_android_system_core_new.git && git cherry-pick 34506a6916549d5c23fa1b1978059c558fafde2c
+croot
 
 #system_update_engine
-#cd system/update_engine
-#git fetch https://github.com/anchalsehrawat/rising_android_system_update_engine.git && git cherry-pick c43c0631ebce21cf4d6e1c9022978d1f937e9214 && git cherry-pick eee2b092e4a0cfcfd10466b376f4d6a7c85f87ca
-#croot
+cd system/update_engine
+git fetch https://github.com/anchalsehrawat/rising_android_system_update_engine.git && git cherry-pick c43c0631ebce21cf4d6e1c9022978d1f937e9214 && git cherry-pick eee2b092e4a0cfcfd10466b376f4d6a7c85f87ca
+croot
 
 #bootable_recovery
-#cd bootable/recovery
-#git fetch https://github.com/anchalsehrawat/rising_android_bootable_recovery.git && git cherry-pick 106bbd05ac4640a1876cb7caca13172dcc82f250 && git cherry-pick 778a3bc4c6e56a939d05803ba5ed95280e9a505a
-#croot
+cd bootable/recovery
+git fetch https://github.com/anchalsehrawat/rising_android_bootable_recovery.git && git cherry-pick 106bbd05ac4640a1876cb7caca13172dcc82f250 && git cherry-pick 778a3bc4c6e56a939d05803ba5ed95280e9a505a
+croot
 
 #hardware_qcom_audio
-#cd hardware/qcom/audio
-#git fetch https://github.com/pjgowtham/android_hardware_qcom_audio.git && git cherry-pick 0580d08da7ab1f87ef64aea8210cebcc2b3bbade && git cherry-pick 7b35659dba676b9b703c0b3e25932985f2f34f2a
-#croot
+cd hardware/qcom-caf/sm8350/audio
+git fetch https://github.com/anchalsehrawat/android_hardware_qcom_audio.git && git cherry-pick a99b3b29e532e94fbf36bcfb4a3ed518e311056c && git cherry-pick 096bf59e6d6ae1e1754bbaabe79e3d3901a149d6
+croot
 echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
