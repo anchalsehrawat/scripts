4ccf97993de5717bb1f28ac05a7bb4b80128b66f
anchalsehrawat <anchalsingh987@gmail.com>
2025-02-11 20:46:42 +0530

    update cherry-picks

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 #Build Script for ziti (OPNCE3)
 #Remove files
 rm -rf .repo/local_manifests
-rm -rf out/target/product/ziti
-rm -rf hardware/lineage/compat
 rm -rf system/core
 rm -rf system/update_engine
 rm -rf bootable/recovery
+rm -rf hardware/lineage/compat
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
@@ -27,25 +26,27 @@ echo "---------------BUILD ENVIRONMENT------------------"
 
 #Cherry-picks
 
-#hardware_lineage_compat
-cd hardware/lineage/compat
-git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick  9144e5ff7fc4b634e10d4916a3ff6a3ba746af39
-croot
-
 #system_core
 cd system/core
-git fetch https://github.com/anchalsehrawat/rising_android_system_core_new.git && git cherry-pick 34506a6916549d5c23fa1b1978059c558fafde2c
-croot
-
-#system_update_engine
-cd system/update_engine
-git fetch https://github.com/anchalsehrawat/rising_android_system_update_engine.git && git cherry-pick c43c0631ebce21cf4d6e1c9022978d1f937e9214 && git cherry-pick eee2b092e4a0cfcfd10466b376f4d6a7c85f87ca
+git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
 croot
 
 #bootable_recovery
 cd bootable/recovery
-git fetch https://github.com/anchalsehrawat/rising_android_bootable_recovery.git && git cherry-pick 106bbd05ac4640a1876cb7caca13172dcc82f250 && git cherry-pick 778a3bc4c6e56a939d05803ba5ed95280e9a505a
+git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick a3ce1a7dd6af031285c1551eda7dde5fb0b1e43f && git cherry-pick 7f64946ca76279ef31e455c9d742b8fa12567377
 croot
+
+#system_update_engine
+cd system/update_engine
+git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick 0edfe05fdc2a3ebb387b827d6728496e2fa7e943 && git cherry-pick 7c48159420c99f1cfb846eebc7d7ee7b1eb167d3
+croot
+
+#hardware_lineage_compat
+cd hardware/lineage/compat
+git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick  4a3e05b445745110ec17b89e3976645744bcacf0
+croot
+
+echo "---------------CHERRY-PICKS DONE------------------"
 
 #Lunch
 breakfast ziti && make installclean && mka bacon -j$(nproc --all)
