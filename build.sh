dc85a3a424b7b56ba53c28cbae58c27584a49776
anchalsehrawat <anchalsingh987@gmail.com>
2025-02-20 17:22:50 +0530

    Update for OTA

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@
 #rm -rf system/update_engine
 #rm -rf bootable/recovery
 #rm -rf hardware/qcom-caf/sm8350/audio
-#rm -rf evolution/OTA-VANILLA
-#rm -rf evolution/OTA
-#rm -rf packages/apps/Updater
+rm -rf evolution/OTA-VANILLA
+rm -rf evolution/OTA
+rm -rf packages/apps/Updater
 rm -rf .repo/local_manifests
 echo "----------------DELETED DIRECTORIES----------------"
 
