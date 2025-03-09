7f3da0cb8dc02d1a38a5a168b8e9d73b9c3465d5
anchalsehrawat <anchalsingh987@gmail.com>
2025-03-09 09:08:44 +0530

    switch to script

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 #Build Script for ziti (OPNCE3)
 #Remove files
-#rm -rf hardware/lineage/compat
-#rm -rf system/core
-#rm -rf system/update_engine
-#rm -rf bootable/recovery
-#rm -rf hardware/qcom-caf/sm8350/audio
+rm -rf hardware/lineage/compat
+rm -rf system/core
+rm -rf system/update_engine
+rm -rf bootable/recovery
+rm -rf hardware/qcom-caf/sm8350/audio
 #rm -rf evolution/OTA-VANILLA
 #rm -rf evolution/OTA
 rm -rf packages/apps/Updater
@@ -45,30 +45,30 @@ croot
 #git fetch https://github.com/anchalsehrawat/Evox_OTA.git && git cherry-pick e976e7590932a077c8e3816591616ab6360d27c1
 #croot
 
-#system_core
-#cd system/core
-#git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
-#croot
+system_core
+cd system/core
+git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
+croot
 
 #bootable_recovery
-#cd bootable/recovery
-#git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick a3ce1a7dd6af031285c1551eda7dde5fb0b1e43f && git cherry-pick 7f64946ca76279ef31e455c9d742b8fa12567377
-#croot
+cd bootable/recovery
+git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick a3ce1a7dd6af031285c1551eda7dde5fb0b1e43f && git cherry-pick 7f64946ca76279ef31e455c9d742b8fa12567377
+croot
 
 #hardware_lineage_compat
-#cd hardware/lineage/compat
-#git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick 4a3e05b445745110ec17b89e3976645744bcacf0
-#croot
+cd hardware/lineage/compat
+git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick 4a3e05b445745110ec17b89e3976645744bcacf0
+croot
 
 #system_update_engine
-#cd system/update_engine
-#git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick 0edfe05fdc2a3ebb387b827d6728496e2fa7e943 && git cherry-pick 7c48159420c99f1cfb846eebc7d7ee7b1eb167d3
-#croot
+cd system/update_engine
+git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick 0edfe05fdc2a3ebb387b827d6728496e2fa7e943 && git cherry-pick 7c48159420c99f1cfb846eebc7d7ee7b1eb167d3
+croot
 
 #hardware_qcom_audio
-#cd hardware/qcom-caf/sm8350/audio
-#git fetch https://github.com/anchalsehrawat/android_hardware_qcom_audio.git && git cherry-pick 55c1bb4d439b725ba1eb92646bac440ccf52925b
-#croot
+cd hardware/qcom-caf/sm8350/audio
+git fetch https://github.com/anchalsehrawat/android_hardware_qcom_audio.git && git cherry-pick 55c1bb4d439b725ba1eb92646bac440ccf52925b
+croot
 echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
