3b3bc3fabeb3dba02ef4cbc4b2a8bc3124472487
anchalsehrawat <anchalsingh987@gmail.com>
2025-04-06 13:25:10 +0530

    Revert "test torch"
    This reverts commit 4c5da2e8b0baedfe02bdb4a8fcf9e27644d99c3a.

--- build.sh
+++ build.sh
@@ -2,30 +2,28 @@
 
 #Build Script for ziti (OPNCE3)
 #Remove files
-#rm -rf hardware/lineage/compat
-#rm -rf system/core
-#rm -rf system/update_engine
-#rm -rf bootable/recovery
-#rm -rf packages/apps/Updater
-#rm -rf .repo/local_manifests
+rm -rf hardware/lineage/compat
+rm -rf system/core
+rm -rf system/update_engine
+rm -rf bootable/recovery
+rm -rf packages/apps/Updater
+rm -rf .repo/local_manifests
 #rm -rf hardware/qcom-caf/sm8350/audio
 #rm -rf evolution/OTA-VANILLA
 #rm -rf evolution/OTA
-rm -rf device/oneplus/sm8350-common
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-#repo init -u https://github.com/Evolution-X/manifest -b vic --depth 1 --git-lfs
-#echo "--------------REPO INITIALISED---------------"
+repo init -u https://github.com/Evolution-X/manifest -b vic --depth 1 --git-lfs
+echo "--------------REPO INITIALISED---------------"
 
 #Local Manifest
-#git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-15 .repo/local_manifests
-git clone https://github.com/anchalsehrawat/android_device_oneplus_sm8350-common.git --depth 1 -b evox-test device/oneplus/sm8350-common
+git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-15 .repo/local_manifests
 echo "-----------------CLONED local manifest-------------------"
 
 #Resync
-#/opt/crave/resync.sh
-#echo "---------------RESYNCED-----------------"
+/opt/crave/resync.sh
+echo "---------------RESYNCED-----------------"
 #Build Environment
 . build/envsetup.sh
 echo "---------------BUILD ENVIRONMENT------------------"
@@ -33,29 +31,29 @@ echo "---------------BUILD ENVIRONMENT------------------"
 #Cherry-picks
 
 #Apps Updater
-#cd packages/apps/Updater
-#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 035d867e3a250803e777c98f3f94d42a0eb3eefa
-#croot
+cd packages/apps/Updater
+git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 035d867e3a250803e777c98f3f94d42a0eb3eefa
+croot
 
-#system_core
-#cd system/core
-#git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
-#croot
+system_core
+cd system/core
+git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
+croot
 
 #bootable_recovery
-#cd bootable/recovery
-#git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick 7f64946ca76279ef31e455c9d742b8fa12567377
-#croot
+cd bootable/recovery
+git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick 7f64946ca76279ef31e455c9d742b8fa12567377
+croot
 
 #hardware_lineage_compat
-#cd hardware/lineage/compat
-#git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick 4a3e05b445745110ec17b89e3976645744bcacf0
-#croot
+cd hardware/lineage/compat
+git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick 4a3e05b445745110ec17b89e3976645744bcacf0
+croot
 
 #system_update_engine
-#cd system/update_engine
-#git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick 0edfe05fdc2a3ebb387b827d6728496e2fa7e943 && git cherry-pick 7c48159420c99f1cfb846eebc7d7ee7b1eb167d3
-#croot
+cd system/update_engine
+git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick 0edfe05fdc2a3ebb387b827d6728496e2fa7e943 && git cherry-pick 7c48159420c99f1cfb846eebc7d7ee7b1eb167d3
+croot
 
 #hardware_qcom_audio
 #cd hardware/qcom-caf/sm8350/audio
@@ -72,13 +70,7 @@ echo "---------------BUILD ENVIRONMENT------------------"
 #git fetch https://github.com/anchalsehrawat/Evox_OTA.git && git cherry-pick e976e7590932a077c8e3816591616ab6360d27c1
 #croot
 
-#Apps Updater
-cd packages/apps/Updater
-#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick e2f5947a6f60075d1983e9be915c54743c5d22e3
-git revert e2f5947a6f60075d1983e9be915c54743c5d22e3
-croot
-
-#echo "----------------CHERRY-PICKS DONE-----------------"
+echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
 lunch lineage_ziti-bp1a-userdebug
@@ -94,14 +86,14 @@ echo "--------------MOVED GAPPS BUILD TO ROOT DIRECTORY--------------"
 
 #Build Vanilla
 #Apps Updater
-#cd packages/apps/Updater
-#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick e2f5947a6f60075d1983e9be915c54743c5d22e3
-#croot
+cd packages/apps/Updater
+git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick e2f5947a6f60075d1983e9be915c54743c5d22e3
+croot
 
-#export WITH_GMS=false
-#m evolution
+export WITH_GMS=false
+m evolution
 
-#mv out/target/product/ziti/*.zip .
-#mv out/target/product/ziti/ziti.json .
-#mv ziti.json ziti_vanilla.json
-#echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
+mv out/target/product/ziti/*.zip .
+mv out/target/product/ziti/ziti.json .
+mv ziti.json ziti_vanilla.json
+echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
