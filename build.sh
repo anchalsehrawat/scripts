d28e51a3296d18007f5dd36a5fa9b35233cc55f2
anchalsehrawat <anchalsingh987@gmail.com>
2025-07-04 13:54:47 +0530

    test

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ rm -rf system/update_engine
 rm -rf bootable/recovery
 rm -rf packages/apps/Updater
 rm -rf .repo/local_manifests
+rm -rf prebuilts/clang/host/linux-x86
 
 echo "----------------DELETED DIRECTORIES----------------"
 
