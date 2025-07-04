fc62cd41e718ec2a2c9bf2fa68981f0f721639e3
anchalsehrawat <anchalsingh987@gmail.com>
2025-07-04 08:46:26 +0530

    script: adb secure

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ echo "----------------CHERRY-PICKS DONE-----------------"
 lunch lineage_ziti-bp1a-userdebug
 
 #ADB 
-export WITH_ADB_INSECURE=true
+#export WITH_ADB_INSECURE=true
 
 #Build GMS
 export WITH_GMS=true
