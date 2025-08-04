dd08c17988127cbbbd3c2b56e8fb4847df7536c4
anchalsehrawat <anchalsingh987@gmail.com>
2025-08-04 19:56:39 +0530

    intial yaap bringup

--- build.sh
+++ build.sh
@@ -6,18 +6,17 @@ rm -rf hardware/lineage/compat
 rm -rf system/core
 rm -rf system/update_engine
 rm -rf bootable/recovery
-rm -rf packages/apps/Updater
 rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
 
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-repo init -u https://github.com/Evolution-X/manifest -b bka --depth 1 --git-lfs
+repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
 echo "--------------REPO INITIALISED---------------"
 
 #Local Manifest
-git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16 .repo/local_manifests
+git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b yaap-16 .repo/local_manifests
 echo "-----------------CLONED local manifest-------------------"
 
 #Resync
@@ -29,11 +28,6 @@ echo "---------------BUILD ENVIRONMENT------------------"
 
 #Cherry-picks
 
-#Apps Updater
-cd packages/apps/Updater
-git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 13f622049a1818e0f5449180e4de51c47afcb2df
-croot
-
 #system_core
 cd system/core
 git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick d3b3d0a378a8af6d79ab0e0abeb78f7f788d804a
@@ -41,7 +35,7 @@ croot
 
 #bootable_recovery
 cd bootable/recovery
-git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick 611906a69e81ed03f6a2b3137257d952f7869b20 && git cherry-pick be79661dfd509f0ff47b0663d097c3ae79e6f666 && git cherry-pick 92eb57ab11fad248257c1cf01d642ed38ae2c888
+git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick be79661dfd509f0ff47b0663d097c3ae79e6f666 && git cherry-pick 92eb57ab11fad248257c1cf01d642ed38ae2c888
 croot
 
 #hardware_lineage_compat
@@ -57,28 +51,19 @@ croot
 echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
-lunch lineage_ziti-bp2a-userdebug
-
-#ADB 
-#export WITH_ADB_INSECURE=true
+lunch yaap_ziti-userdebug
 
 #Build GMS
 export WITH_GMS=true
-m evolution
+m yaap
 
 mv out/target/product/ziti/*.zip .
 mv out/target/product/ziti/ziti.json .
-mv ziti.json ziti_gapps.json
+mv ziti.json ziti_gms.json
 echo "--------------MOVED GAPPS BUILD TO ROOT DIRECTORY--------------"
 
-#Build Vanilla
-#Apps Updater
-#cd packages/apps/Updater
-#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 747f361e516fe6267293a59e79c8ff7a0dd7b6f5
-#croot
-
 export WITH_GMS=false
-m evolution
+m yaap
 
 mv out/target/product/ziti/*.zip .
 mv out/target/product/ziti/ziti.json .
