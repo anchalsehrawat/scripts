b160c7582d893476d6ad6edfb30aebccdd81e55b
anchalsehrawat <anchalsingh987@gmail.com>
2025-09-16 09:39:31 +0530

    rm -rf audio for missing header

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 rm -rf hardware/lineage/compat
 rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
+rm -rf hardware/qcom-caf/sm8350/audio
 
 echo "----------------DELETED DIRECTORIES----------------"
 
