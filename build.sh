60d46435555c53bad287cdea6d407db9851e407e
anchalsehrawat <anchalsingh987@gmail.com>
2025-09-16 20:42:47 +0530

    add OTA support

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@
 rm -rf hardware/lineage/compat
 rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
+rm -rf vendor/yaap
+rm -rf packages/apps/OpenDelta
+
 
 echo "----------------DELETED DIRECTORIES----------------"
 
@@ -24,6 +27,15 @@ echo "---------------RESYNCED-----------------"
 echo "---------------BUILD ENVIRONMENT------------------"
 
 #Cherry-picks
+#OTA Support
+cd vendor/yaap
+git fetch https://github.com/anchalsehrawat/vendor_yaap.git && git cherry-pick 74eeddb39c9ccf66d0cf00dd21cfb44f7136f794 && git cherry-pick f20efeb17d78268f3b922af169f6de6b6070ba8e
+croot
+
+cd packages/apps/OpenDelta
+git fetch https://github.com/anchalsehrawat/packages_apps_OpenDelta.git && git cherry-pick 9446690c27b0975d28e590ce068cfacddf880e3e
+croot
+
 #hardware_lineage_compat
 cd hardware/lineage/compat
 git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git && git cherry-pick 4a3e05b445745110ec17b89e3976645744bcacf0
