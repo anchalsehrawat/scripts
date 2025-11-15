79b1962b8eef8683d01b250196190733b7386dfa
anchalsehrawat <anchalsingh987@gmail.com>
2025-11-15 13:02:48 +0530

    update

--- build.sh
+++ build.sh
@@ -86,6 +86,11 @@ export WITH_GMS=true
 m evolution
 
 mv out/target/product/ziti/*.zip .
+mv out/target/product/ziti/boot.img .
+mv out/target/product/ziti/dtbo.img .
+mv out/target/product/ziti/vbmeta.img .
+mv out/target/product/ziti/vendor_boot.img .
+mv out/target/product/ziti/super_empty.img .
 mv out/target/product/ziti/ziti.json .
 mv ziti.json ziti_gms.json
 echo "--------------MOVED GAPPS BUILD TO ROOT DIRECTORY--------------"
