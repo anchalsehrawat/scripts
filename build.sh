55d72fe493f184e59b288eeeae4955b2ab1ab5ec
anchalsehrawat <anchalsingh987@gmail.com>
2025-12-06 10:14:20 +0530

    Update qpr1

--- build.sh
+++ build.sh
@@ -14,11 +14,11 @@ rm -rf vendor/lineage
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-repo init -u https://github.com/Evolution-X/manifest -b bka --depth 1 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq1 --depth 1 --git-lfs
 echo "--------------REPO INITIALISED---------------"
 
 #Local Manifest
-git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16 .repo/local_manifests
+git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16.1 .repo/local_manifests
 echo "-----------------CLONED local manifest-------------------"
 
 #Resync
@@ -75,7 +75,7 @@ echo "-------------Cloned-------------------"
 echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
-lunch lineage_ziti-bp2a-userdebug
+lunch lineage_ziti-bp3a-userdebug
 
 #ADB 
 #export WITH_ADB_INSECURE=true
