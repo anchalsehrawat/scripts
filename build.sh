46a525bb17f9c39e23e5519313d9aab09595f98d
Loid <anchalsingh987@gmail.com>
2026-01-28 22:54:34 +0530

    update

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ echo "-------------Cloned-------------------"
 echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Lunch
-lunch lineage_ziti-bp4a-user
+lunch lineage_ziti-bp4a-userdebug
 
 #ADB 
 #export WITH_ADB_INSECURE=true
@@ -93,8 +93,8 @@ export UNSAFE_DISABLE_HIDDENAPI_FLAGS=true
 
 #Build GMS
 export WITH_GMS=true
-export TARGET_USES_MINI_GAPPS=true
-export TARGET_INCLUDE_LIVE_WALLPAPERS=true
+#export TARGET_USES_MINI_GAPPS=true
+#export TARGET_INCLUDE_LIVE_WALLPAPERS=true
 
 m evolution
 
