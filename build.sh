7ed8f47cf6f694a37fd098c368a9d48da8d3d6a4
Loid <anchalsingh987@gmail.com>
2026-01-31 10:40:03 +0530

    update

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm -rf packages/apps/Updater
 rm -rf .repo/local_manifests
 #rm -rf prebuilts/clang/host/linux-x86
 #rm -rf vendor/lineage
-#rm -rf vendor/evolution-priv/keys
+rm -rf vendor/evolution-priv/keys
 #rm -rf vendor/oplus/camera
 
 echo "----------------DELETED DIRECTORIES----------------"
