c51fbd4932d8042c667bb382db73feef78b91799
Loid <anchalsingh987@gmail.com>
2026-04-02 06:15:42 +0530

    Update

--- build.sh
+++ build.sh
@@ -2,29 +2,24 @@
 
 #Build Script for ziti (OPNCE3)
 #Remove files
-#rm -rf bootable/recovery
-rm -rf hardware/lineage/compat
-#rm -rf system/core
-#rm -rf system/update_engine
-rm -rf packages/apps/Updater
-rm -rf .repo/local_manifests
-#rm -rf prebuilts/clang/host/linux-x86
-#rm -rf vendor/lineage
-#rm -rf vendor/oplus/camera
+#rm -rf hardware/lineage/compat
+#rm -rf packages/apps/Updater
+#rm -rf .repo/local_manifests
+#rm -rf frameworks/base
 
-echo "----------------DELETED DIRECTORIES----------------"
+#echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth 1 --git-lfs
-echo "--------------REPO INITIALISED---------------"
+#repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth 1 --git-lfs
+#echo "--------------REPO INITIALISED---------------"
 
 #Local Manifest
-git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16.2-ksun .repo/local_manifests
-echo "-----------------CLONED local manifest-------------------"
+#git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16.2-ksun .repo/local_manifests
+#echo "-----------------CLONED local manifest-------------------"
 
 #Resync
-/opt/crave/resync.sh
-echo "---------------RESYNCED-----------------"
+#/opt/crave/resync.sh
+#echo "---------------RESYNCED-----------------"
 
 #Build Environment
 . build/envsetup.sh
@@ -32,36 +27,24 @@ echo "---------------BUILD ENVIRONMENT------------------"
 
 #Cherry-picks
 #hardware_lineage_compat
-cd hardware/lineage/compat
-git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git -t sixteen && git cherry-pick 6f6bbb4a686ac367383584a183ebe31ee5d4b0e1 6a8ac15383a76806b807aaf70b3f6d98b907ec02
-croot
-
-#Stock Reverting patches
-#bootable_recovery
-#cd bootable/recovery
-#git fetch https://github.com/anchalsehrawat/evox_bootable_recovery.git && git cherry-pick fc8c79dafadb7176b9b962ab02fcec5b5175c51b && git cherry-pick 62e74cc196b3ce6d1b41a9e11499e46e4ff8aa6e && git cherry-pick 52bbdfbc7e27aecfd5dbd21c571bdbca998d4011
+#cd hardware/lineage/compat
+#git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git -t sixteen && git cherry-pick 6f6bbb4a686ac367383584a183ebe31ee5d4b0e1 6a8ac15383a76806b807aaf70b3f6d98b907ec02
 #croot
 
-#system_core
-#cd system/core
-#git fetch https://github.com/anchalsehrawat/evox_system_core.git && git cherry-pick 978f6b40ba6531490a6c3588f7bb14aa10b279cf
+#cd packages/apps/Updater
+#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 13f622049a1818e0f5449180e4de51c47afcb2df
 #croot
 
-#system_update_engine
-#cd system/update_engine
-#git fetch https://github.com/anchalsehrawat/android_system_update_engine.git && git cherry-pick d804cc2a02e0e94c2d8e9ba47175f3946954306d && git cherry-pick d526f28031438c184746cfe6a038186598180fe6
+#cd frameworks/base
+#git fetch https://github.com/anchalsehrawat/evox_frameworks_base.git -t bq2-fix && git cherry-pick 573fac673693a5d3bdb25ad0767f991f2ec1fbad faed489605a2962d81598db405d8e3608b23b18b
 #croot
-
-#For OTA Updates
-#Apps Updater
-cd packages/apps/Updater
-git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 13f622049a1818e0f5449180e4de51c47afcb2df
-croot
 
 #Vanilla Updater urls
 #cd vendor/lineage
 #git fetch https://github.com/anchalsehrawat/vendor_evolution.git && git cherry-pick 587744521b5af1293dff08f602087f41b9be2add
 #croot
+
+#echo "----------------CHERRY-PICKS DONE-----------------"
 
 #Sign Priv Keys
 #rm -rf vendor/evolution-priv/keys
@@ -70,49 +53,51 @@ croot
 #./keys.sh
 #croot
 
-rm -rf vendor/evolution-priv/keys
-echo "-------------------Removed ex----------------------"
-git clone https://github.com/anchalsehrawat/scripts.git -b ex-qp2 vendor/evolution-priv/keys
-echo "-------------Cloned-------------------"
+#rm -rf vendor/evolution-priv/keys/
+#git clone https://github.com/anchalsehrawat/scripts.git -b exk vendor/evolution-priv/keys
 
-echo "----------------CHERRY-PICKS DONE-----------------"
+#cd vendor/
+#mkdir evolution-priv/
+#cd evolution-priv/
+#rm -rf keys/
+#wget https://github.com/anchalsehrawat/scripts/releases/download/exk/exk.zip
+#unzip exk.zip
+#rm exk.zip
+#croot
+#echo "-------------Cloned k-------------------"
+
+rm -rf kernel/oneplus/sm8350
+git clone https://github.com/anchalsehrawat/android_kernel_oneplus_sm8350 -b los-23.2-ksun-bkp kernel/oneplus/sm8350
 
 #Lunch
 lunch lineage_ziti-bp4a-userdebug
 
 #ADB 
 #export WITH_ADB_INSECURE=true
-export TARGET_INCLUDE_ACCORD=true
-
-#Crave
-export BUILD_USERNAME=Loid
-export BUILD_HOSTNAME=crave
-export TZ=Asia/Kolkata
 #For OPcam
 export UNSAFE_DISABLE_HIDDENAPI_FLAGS=true
 
 #Build GMS
-export TORCH_STR_SUPPORTED=true
 export WITH_GMS=true
 export TARGET_USES_MINI_GAPPS=true
 #export TARGET_INCLUDE_LIVE_WALLPAPERS=true
 
 m evolution
 
+mv out/target/product/ziti/*.zip .
 #mv out/target/product/ziti/boot.img .
 #mv out/target/product/ziti/dtbo.img .
 #mv out/target/product/ziti/vbmeta.img .
 #mv out/target/product/ziti/vendor_boot.img .
 #mv out/target/product/ziti/super_empty.img .
-mv out/target/product/ziti/*.zip .
 #mv out/target/product/ziti/ziti.json .
 #mv ziti.json ziti_gms.json
 echo "--------------MOVED GAPPS BUILD TO ROOT DIRECTORY--------------"
 
-export WITH_GMS=false
-m evolution
+#export WITH_GMS=false
+#m evolution
 
-mv out/target/product/ziti/*.zip .
+#mv out/target/product/ziti/*.zip .
 #mv out/target/product/ziti/ziti.json .
 #mv ziti.json ziti_vanilla.json
 #echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
