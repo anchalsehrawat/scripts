0d275b9b718a200f1bf9e1b67e246bd599a097fd
Loid <anchalsingh987@gmail.com>
2026-04-02 18:14:26 +0530

    Update

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@
 
 #Build Script for ziti (OPNCE3)
 #Remove files
-rm -rf hardware/lineage/compat
-rm -rf packages/apps/Updater
-rm -rf .repo/local_manifests
+#rm -rf hardware/lineage/compat
+#rm -rf packages/apps/Updater
+#rm -rf .repo/local_manifests
 #rm -rf frameworks/base
 
 #echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth 1 --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b bq2 --depth 1 --git-lfs
 #echo "--------------REPO INITIALISED---------------"
 
 #Local Manifest
-git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16.2 .repo/local_manifests
+#git clone https://github.com/anchalsehrawat/local_manifests --depth 1 -b evox-16.2 .repo/local_manifests
 #echo "-----------------CLONED local manifest-------------------"
 
 #Resync
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 #echo "---------------RESYNCED-----------------"
 
 #Build Environment
@@ -27,13 +27,13 @@ echo "---------------BUILD ENVIRONMENT------------------"
 
 #Cherry-picks
 #hardware_lineage_compat
-cd hardware/lineage/compat
-git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git -t sixteen && git cherry-pick 6f6bbb4a686ac367383584a183ebe31ee5d4b0e1 6a8ac15383a76806b807aaf70b3f6d98b907ec02
-croot
+#cd hardware/lineage/compat
+#git fetch https://github.com/anchalsehrawat/android_hardware_lineage_compat.git -t sixteen && git cherry-pick 6f6bbb4a686ac367383584a183ebe31ee5d4b0e1 6a8ac15383a76806b807aaf70b3f6d98b907ec02
+#croot
 
-cd packages/apps/Updater
-git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 13f622049a1818e0f5449180e4de51c47afcb2df
-croot
+#cd packages/apps/Updater
+#git fetch https://github.com/anchalsehrawat/evox_packages_apps_Updater.git && git cherry-pick 13f622049a1818e0f5449180e4de51c47afcb2df
+#croot
 
 #cd frameworks/base
 #git fetch https://github.com/anchalsehrawat/evox_frameworks_base.git -t bq2-fix && git cherry-pick 573fac673693a5d3bdb25ad0767f991f2ec1fbad faed489605a2962d81598db405d8e3608b23b18b
@@ -66,8 +66,8 @@ echo "----------------CHERRY-PICKS DONE-----------------"
 #croot
 #echo "-------------Cloned k-------------------"
 
-#rm -rf kernel/oneplus/sm8350
-#git clone https://github.com/anchalsehrawat/android_kernel_oneplus_sm8350 -b los-23.2-ksun-bkp kernel/oneplus/sm8350
+rm -rf kernel/oneplus/sm8350
+git clone https://github.com/anchalsehrawat/android_kernel_oneplus_sm8350 -b los-23.2-ksun-bkp kernel/oneplus/sm8350
 #rm -rf device/oneplus/sm8350-common
 #git clone https://github.com/anchalsehrawat/android_device_oneplus_sm8350-common -b evox-16.2-test device/oneplus/sm8350-common
 
@@ -80,26 +80,26 @@ lunch lineage_ziti-bp4a-userdebug
 export UNSAFE_DISABLE_HIDDENAPI_FLAGS=true
 
 #Build GMS
-export WITH_GMS=true
-export TARGET_USES_MINI_GAPPS=true
+#export WITH_GMS=true
+#export TARGET_USES_MINI_GAPPS=true
 #export TARGET_INCLUDE_LIVE_WALLPAPERS=true
 
-m evolution
+#m evolution
 
-mv out/target/product/ziti/*.zip .
+#mv out/target/product/ziti/*.zip .
 #mv out/target/product/ziti/boot.img .
 #mv out/target/product/ziti/dtbo.img .
 #mv out/target/product/ziti/vbmeta.img .
 #mv out/target/product/ziti/vendor_boot.img .
 #mv out/target/product/ziti/super_empty.img .
-mv out/target/product/ziti/ziti.json .
-mv ziti.json ziti_gms.json
+#mv out/target/product/ziti/ziti.json .
+#mv ziti.json ziti_gms.json
 echo "--------------MOVED GAPPS BUILD TO ROOT DIRECTORY--------------"
 
-#export WITH_GMS=false
-#m evolution
+export WITH_GMS=false
+m evolution
 
-#mv out/target/product/ziti/*.zip .
+mv out/target/product/ziti/*.zip .
 #mv out/target/product/ziti/ziti.json .
 #mv ziti.json ziti_vanilla.json
 #echo "--------------MOVED VANILLA BUILD TO ROOT DIRECTORY--------------"
